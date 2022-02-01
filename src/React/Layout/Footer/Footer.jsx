b66265f4fc77d4a7123606ac5c75ba97f54c003b
Kyle Behrens <kyle.behrens1@gmail.com>
2022-01-31 19:21:28 -0800

    Added Site Logo

--- src/React/Layout/Footer/Footer.jsx
+++ src/React/Layout/Footer/Footer.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import Logo from './SiteTitle.jsx';
-import SiteTitle from './Logo.jsx';
+import Logo from './Logo.jsx';
+import SiteTitle from './SiteTitle.jsx';
 
 const Footer = () => {
 
--- src/React/Layout/Footer/Logo.jsx
+++ src/React/Layout/Footer/Logo.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import SiteLogo from 'React/common/SiteLogo.jsx';
+
 const Logo = () => {
 
     return (
         <LogoStyled className='Logo'>
-            Logo 
+        <SiteLogo /> 
         </LogoStyled>
     );
 }
 
 export default Logo;
 
-const LogoStyled = styled.div`
-    
+const LogoStyled = styled.div`  
+    svg.hot-tub-icon {
+        width: 30px;     
+    }
 `;
