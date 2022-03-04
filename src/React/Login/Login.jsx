06f8c096ca198dcc70eed918fe1b7025d47a27b8
Kyle Behrens <kyle.behrens1@gmail.com>
2022-03-03 18:34:12 -0800

    Updated Login

--- src/React/Login/Login.jsx
+++ src/React/Login/Login.jsx
@@ -1,29 +1,40 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 
-const Login = () => {
+/* Components ---------------------------*/
+import UniversalForm, { Input, Textarea, SubmitButton } from 'React/common/UniversalForm/UniversalForm.jsx';
 
-    const [username, usernameUpdate] = useState('');
+const Login = ({children, id, label}) => {
 
-    const handleChange = (e) => {
-        console.log('User Wrote' , e.target.value);
-        usernameUpdate(e.target.value);
-    } 
+    const onSubmitHandler = ({fields, payload}) => {
+        console.log('onSubmit, fields, payload' , fields, payload);
+    }
 
     return (
         <LoginStyled className='Login'>
-            <h1>Login</h1> 
-
-            <input
-                value={ username }
-                onChange={ handleChange }
-            />
-
-
-
+            <h1>Login</h1>
+            <UniversalForm
+                apiUrl='/user/login'
+                onSubmit={ onSubmitHandler }
+            >
+                <Input 
+                    label='Username'
+                    id='username'
+                    placeholder="user@domain.com"
+                    rules={ ['required'] }
+                />
+                <Input 
+                    label='Password'
+                    id='password'
+                    type='password'
+                    rules={ ['required'] }
+                />
+                <SubmitButton>Log in</SubmitButton>
+            </UniversalForm>
         </LoginStyled>
     );
 }
+
 
 export default Login;
 
